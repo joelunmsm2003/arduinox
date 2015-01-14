8f4536645134856c990ec4528dfb5ad20894813f
andy <joelunmsm@gmail.com>
2015-01-14 11:17:52 -0500

    eliminar

--- matriz/matriz.ino
+++ matriz/matriz.ino
@@ -6,41 +6,7 @@ int ciclo = 0;         //Variable para los ciclos de cada letra en cada posicion
 int desplaza = 0;      //Variable para generar desplazamiento en las filas
 //Definimos los numeros decimales que hacen falta para dibujar cada caracter
 #define SP {0, 0, 0, 0, 0}               //Espacio
-#define EX {0, 125, 0, 0, 0}             //Exclamacion !
-#define A {31, 36, 68, 36, 31}
-#define B {127, 73, 73, 73, 54,}
-#define C {62, 65, 65, 65, 34}
-#define D {127, 65, 65, 34, 28}
-#define E {127, 73, 73, 65, 65}
-#define F {127, 72, 72, 72, 64}
-#define G {62, 65, 65, 69, 38}
-#define H {127, 8, 8, 8, 127}
-#define I {0, 65, 127, 65, 0}
-#define J {2, 1, 1, 1, 126}
-#define K {127, 8, 20, 34, 65}
-#define L {127, 1, 1, 1, 1}
-#define M {127, 32, 16, 32, 127}
-#define N {127, 32, 16, 8, 127}
-#define O {62, 65, 65, 65, 62}
-#define P {127, 72, 72, 72, 48}
-#define Q {62, 65, 69, 66, 61}
-#define R {127, 72, 76, 74, 49}
-#define S {50, 73, 73, 73, 38}
-#define T {64, 64, 127, 64, 64}
-#define U {126, 1, 1, 1, 126}
-#define V {124, 2, 1, 2, 124}
-#define W {126, 1, 6, 1, 126}
-#define X {99, 20, 8, 20, 99}
-#define XA {36, 00, 255, 66, 60}
-#define XB {36, 00, 60, 66, 255}
-#define Y {96, 16, 15, 16, 96}
-#define Z {67, 69, 73, 81, 97}
-//#define FD {00, 28, 28, 127, 62, 28, 8, 00}
-//#define FI {00, 8, 28, 62, 127, 28, 28, 00}
-#define FX {255, 255, 255, 255, 255, 255, 255, 255}
-#define FY {255, 255, 255, 231, 231, 255, 255, 255}
-#define FZ {255, 129, 189, 165, 165, 189, 129, 255}
-#define CA {116,86, 117, 5, 5, 117, 86, 116}
+
 //Escribimos la frase separando cada letra por comas
 //En el primer numero lo adaptaremos la longitud de la frase (caracteres)
 //byte frase[32][6]={B,I,E,N,V,E,N,I,D,O,S,SP,A,SP,A,R,D,U,I,N,O,SP,A,C,A,D,E,M,Y,SP,SP,SP};
